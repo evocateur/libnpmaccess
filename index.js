0f65f60323e1b2575a87e65f32a733e9d47bb805
Daniel Stockman <daniel.stockman@gmail.com>
2019-05-08 12:56:07 -0700

    feat(deps): Use forked npm-registry-fetch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const figgyPudding = require('figgy-pudding')
 const getStream = require('get-stream')
 const npa = require('npm-package-arg')
-const npmFetch = require('npm-registry-fetch')
+const npmFetch = require('@evocateur/npm-registry-fetch')
 const {PassThrough} = require('stream')
 const validate = require('aproba')
 
